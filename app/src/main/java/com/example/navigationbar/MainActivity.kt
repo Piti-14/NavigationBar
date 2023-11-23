4c0f81b1be5263a4f44db7f7c90842e0f59b8880
Piti-14 <alupit526@ieselcaminas.org>
2023-11-23 20:51:22 +0100

    Encabezado

--- app/src/main/java/com/example/navigationbar/MainActivity.kt
+++ app/src/main/java/com/example/navigationbar/MainActivity.kt
@@ -3,13 +3,30 @@ package com.example.navigationbar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CameraAlt
+import androidx.compose.material.icons.filled.Coffee
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.LightMode
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.navigationbar.ui.theme.NavigationBarTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +39,29 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column(Modifier.fillMaxSize()) {
+                        Text(
+                            text = "MY PROJECTs",
+                            modifier = Modifier.fillMaxWidth().height(670.dp)
+                        )
+
+                        var selectedItem by rememberSaveable { mutableStateOf(0) }
+                        val items = listOf("My Photos", "Coffe Shops", "Sol")
+                        val icons = listOf(Icons.Filled.CameraAlt, Icons.Filled.Coffee, Icons.Filled.LightMode)
+
+                        NavigationBar(Modifier.weight(1f)) {
+                            items.forEachIndexed { index, item ->
+                                NavigationBarItem(
+                                    icon = { icons[index] },
+                                    label = { Text(item) },
+                                    selected = selectedItem == index,
+                                    onClick = { selectedItem = index }
+                                )
+                            }
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NavigationBarTheme {
-        Greeting("Android")
     }
 }
